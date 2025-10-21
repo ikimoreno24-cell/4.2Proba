807a75c0da351289ce0fcc0cc292b4b12e547948
iker <ikimoreno24@lhusurbil.eus>
2025-10-21 10:22:47 +0200

    Prograa bukatuta kommit-a

---
+++ src/Programa.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+public class Programa {
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        int saila = 0;
+        boolean menu = true;
+        String ofizioa = "programatzailea";
+        String ofizioa1 = "analista";
+        String ofizioa3 = "diseinatzailea";
+
+
+        while (menu == true){
+            
+            System.out.println("Sartu langile zenbakia");
+            int zenb = sc.nextInt();
+            
+
+            if (zenb < 100 || zenb > 999){
+                System.out.println("ER1: langile kode okerra");
+            }
+            else {
+                System.out.println("Sartu langilearen saila");
+                saila = sc.nextInt();
+                if ( saila < 10 || saila >99){
+                    System.out.println("ER2: Saila ez da zuzena");
+                }
+                else{
+                    System.out.println("Sartu ofizioa: programatzailea, analista edo diseinatzailea (dena minuskulaz idatzita)");
+                    sc.nextLine();
+                    String lana = sc.nextLine();
+                    if (lana.equals("programatzailea")){
+                        System.out.println("programatzailearen soldata 2000€-koa da.");
+                        menu = false;
+                    } 
+                    else if(lana.equals("analista")){
+                        System.out.println("analistaren soldata 2500€-koa da.");
+                        menu = false;
+                    }
+                    else if(lana.equals("diseinatzailea")){
+                        System.out.println("diseinatzailearen soldata 1500€-koa da.");
+                        menu = false;
+                    }
+                    else{
+                        System.out.println("ER3: ofiziorik ez da aukeratu");
+                    }
+                        
+                    
+                   
+
+                }
+                 
+                
+            }
+            
+        }
+
+
+    }
+
+}
